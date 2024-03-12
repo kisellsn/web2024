c94560b5d7c704a9f2920e40455b049b1fc6a980
kisellsn <sonyakondratskaya@gmail.com>
2024-03-13 00:29:31 +0100

    add lab2 report

--- main.js
+++ main.js
@@ -1,82 +1,82 @@
-const asideButtonsBlock = document.getElementById("aside_buttons");
-const destinationHTML = document.getElementById("inserted");
-const extraCssLink = document.getElementById("extra_css"); // додатковий CSS вкладених блоків
+const asideButtonsContainer = document.getElementById("aside_buttons");
+const insertionPoint = document.getElementById("inserted");
+const additionalCssLink = document.getElementById("extra_css");
 
-const fadeIn = [{ opacity: 0}, { opacity: 0.33 },{ opacity: 0.66 }, { opacity: 1}];    // константи для анімації
-const timing = { duration: 300, iterations: 1 };                                         //
+const fadeInAnimation = [{ opacity: 0}, { opacity: 0.33 },{ opacity: 0.66 }, { opacity: 1}];
+const animationTiming = { duration: 300, iterations: 1 };
 
-let site_data = getSiteData(); // структура з structure.json
-let current_lb = {};           // об'єкт поточної лаби
-let current_lb_btn = null;
-let current_aside_btn = null;
+let siteData = fetchSiteData();
+let currentLab = {};
+let currentLabButton = null;
+let currentAsideButton = null;
 
-async function insertBlock(sourceHTML, linkCSS = "") {
-  let myObject = await fetch(sourceHTML);
-  if (myObject.status == 200) {
-    destinationHTML.innerHTML = await myObject.text();
-    destinationHTML.animate(fadeIn, timing);
-    extraCssLink.href = linkCSS;
+async function insertContent(htmlSource, cssLink = "") {
+  let response = await fetch(htmlSource);
+  if (response.status == 200) {
+    insertionPoint.innerHTML = await response.text();
+    insertionPoint.animate(fadeInAnimation, animationTiming);
+    additionalCssLink.href = cssLink;
   } else {
-    destinationHTML.innerHTML = `<p class="loading">${myObject.statusText}</p> <br><p class="loading">Sorry, the file has not been added yet</p>`;
-    extraCssLink.href = "";
+    insertionPoint.innerHTML = `<p class="loading">${response.statusText}</p> <br><p class="loading">Sorry, the file has not been added yet</p>`;
+    additionalCssLink.href = "";
   }
 }
-async function getSiteData(source = "/web2024/structure.json") {
+
+async function fetchSiteData(source = "/web2024/structure.json") {
   const response = await fetch(source);
   const { data } = await response.json();
-  site_data = data;
+  siteData = data;
 }
 
 for (button of document.querySelectorAll("button[id^='lb']")) {
-  button.addEventListener("click", lbButtonHandler);
+    button.addEventListener("click", labButtonHandler);
 }
 
-function setCurrentBtnActive(old_button, new_btn) {
-  if (old_button) {
-   old_button.parentElement.classList.remove("active");
-  }
-  new_btn.parentElement.classList.add("active");
+function setActiveButton(previousButton, newButton) {
+    if (previousButton) {
+        previousButton.parentElement.classList.remove("active");
+    }
+    newButton.parentElement.classList.add("active");
 }
 
-function lbButtonHandler(event) {
-  current_lb = site_data.labs[event.target.id];
-  setCurrentBtnActive(current_lb_btn, event.target);
-  current_lb_btn = event.target;
-  asideButtonsBlock.innerHTML = "";
-  extraCssLink.href = "";
-  for (button in current_lb.buttons) {
-    createAsideButton(button);
-  }
-  if (asideButtonsBlock.firstChild) {
-    asideButtonsBlock.style.display = "flex";
-    asideButtonsBlock.firstChild.firstChild.click();
-  } else {
-    destinationHTML.innerHTML = '<p class="loading">Sorry, the file has not been added yet</p>';
-    asideButtonsBlock.style.display = "none";
-  }
+function labButtonHandler(event) {
+    currentLab = siteData.labs[event.target.id];
+    setActiveButton(currentLabButton, event.target);
+    currentLabButton = event.target;
+    asideButtonsContainer.innerHTML = "";
+    additionalCssLink.href = "";
+    for (button in currentLab.buttons) {
+        createAsideButton(button);
+    }
+    if (asideButtonsContainer.firstChild) {
+        asideButtonsContainer.style.display = "flex";
+        asideButtonsContainer.firstChild.firstChild.click();
+    } else {
+        insertionPoint.innerHTML = '<p class="loading">Sorry, the file has not been added yet</p>';
+        asideButtonsContainer.style.display = "none";
+    }
 }
 
-function createAsideButton(button_name) {
-  let div_button = document.createElement("div");
-  let button_data = current_lb.buttons[button_name];
-  div_button.classList.add("nav-item");
-  let button = `<button id=${button_name} class="button" type="button"> ${button_data.title}</button>`;
-  div_button.innerHTML = button;
-  let html_file = current_lb.rel_path + button_data.html_file;
-  let css_file = current_lb.rel_path + button_data.css_file;
-  let files = { html_file, css_file };
-  div_button.firstChild.addEventListener(
-    "click",
-    asideButtonHandler.bind(files)
-  );
-  asideButtonsBlock.append(div_button);
+function createAsideButton(buttonName) {
+    let buttonContainer = document.createElement("div");
+    let buttonData = currentLab.buttons[buttonName];
+    buttonContainer.classList.add("nav-item");
+    let button = `<button id=${buttonName} class="button" type="button"> ${buttonData.title}</button>`;
+    buttonContainer.innerHTML = button;
+    let htmlFile = currentLab.rel_path + buttonData.html_file;
+    let cssFile = currentLab.rel_path + buttonData.css_file;
+    let files = { htmlFile, cssFile };
+    buttonContainer.firstChild.addEventListener(
+      "click",
+      asideButtonHandler.bind(files)
+    );
+    asideButtonsContainer.append(buttonContainer);
 }
 
 async function asideButtonHandler(event) {
-
-  await insertBlock(this.html_file, this.css_file);
-  setCurrentBtnActive(current_aside_btn, event.target);
-  current_aside_btn = event.target;
-  current_aside_btn.parentElement.classList.add("active");
-  PR.prettyPrint(); // бібліотека code-prettify - автоматично розфарбовує код в стилі IDE
+  await insertContent(this.htmlFile, this.cssFile);
+  setActiveButton(currentAsideButton, event.target);
+  currentAsideButton = event.target;
+  currentAsideButton.parentElement.classList.add("active");
+  PR.prettyPrint();
 }
